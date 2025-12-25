51df6dd995acb114b083ca950a663b29fd065db1
krishnashukla1 <120076833+krishnashukla1@users.noreply.github.com>
2025-12-25 06:14:58 +0530

    .env add

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,27 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:5000/api", 
+// });
+
+// // Add token automatically
+// API.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default API;
+
+
+
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", 
+  baseURL: import.meta.env.VITE_BASE_URL,
 });
 
 // Add token automatically
